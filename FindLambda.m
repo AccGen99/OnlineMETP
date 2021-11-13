4bd3e52151d343ffe428de5263f7d27ef8634907
AccGen99 <72756584+AccGen99@users.noreply.github.com>
2021-11-13 00:46:35 -0800

    Calculate LagrangeVec for velocity state vector

---
+++ FindLambda.m
@@ -0,0 +1,3 @@
+function val = FindLambda(w2, A, x_dot, k2, w1, Q, x_dot_dot, C, omega, R, R_dot)
+    val = w2*(A*x_dot)/k2;% - 2*w1*(A^2)*inv(Q)*x_dot_dot - 2*w1*A*C*inv(Q)*x_dot/(k2^2) + 2*w1*omega*(A^2)*inv(Q)*R*R_dot'*x_dot;
+end 
