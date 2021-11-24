bb5a7b6905b3386ef04f904fc8f6b443607115fc
AccGen99 <72756584+AccGen99@users.noreply.github.com>
2021-11-24 14:09:31 +0530

    Corrections

--- Params_Calcs.m
+++ Params_Calcs.m
@@ -3,6 +3,7 @@ phi = [0, pi]; % phi(1) is initial and phi(2) is final
 xf = 0.3; % Final x-coordinate
 zi = [0, 0, phi(1), 0, 0, 0]';
 zf = [xf, 0, phi(2), 0, 0, 0]';
+alpha_x = -1;
 
 % System parameter values
 Fv = 0.0025; % (N.m/(rad/sec))
@@ -18,15 +19,15 @@ Ra = 7.9;       % ohm
 n = 1;          % Gear ratio - motor & wheel
 
 % Calculation of some constants
-M = Mc + 3*Mw;
+M_total = Mc + 3*Mw;
 Jw = 0.5*Mw*r^2;
 Jc = 0.5*Mc*L^2;
-J = 3*Jw + Jc;
+J = 3*(Jw+0.5*Mw*L^2) + Jc;
 B = [0, 1, L; -sin(pi/3), -cos(pi/3), L; sin(pi/3), -cos(pi/3), L];
-M = diag([M, M, J]);
+M = diag([M_total, M_total, J]);
 k2 = Vs*Kt*n/(Ra*r);
 D = k2*B';
-k1 = 1.5*(Fv+Kt*Kb*n^2/Ra)/r^2;
+k1 = 1.5*(Fv+Kt*Kb*(n^2)/Ra)/r^2;
 C = k1*diag([1, 1, 2*L^2]);
 Q = B'*B;
 A = M+Jw*Q/r^2;
@@ -36,7 +37,7 @@ w2 = Kb*n*Vs/(Ra*r);
 % For differential equation of phi-dot
 phi_f = phi(2);
 C3 = (C(3,3)/A(3,3))^2 - k2*w2*Q(3,3)*C(3,3)/(w1*A(3,3)^2);
-C4 = Q(3,3)*k2^2/(2*w1*(A(3,3))^2);
+C4 = Q(3,3)*(k2^2)/(2*w1*((A(3,3))^2));
 h3 = 3*(L^2)*(Fv+Kt*Kb*(n^2)/Ra)/r^2;
 k = (h3^2 - 3*(L^2)*h3*Kt*Kb*(n^2)/(Ra*(r^2)))/(Jc + (3*Jw*L^2)/r^2)^2;
 tau_w = 1/(k^0.5);
@@ -46,6 +47,7 @@ tau_w = 1/(k^0.5);
 
 % For differential equation of x_dot
 R = [cos(phi(1)), -sin(phi(1)), 0; sin(phi(1)), cos(phi(1)), 0; 0, 0, 1];
-R_dot = [-sin(phi(1)), -cos(phi(1)), 0; cos(phi(1)), -sin(phi(1)), 0; 0, 0, 0];
+R_dot = [-sin(phi(1)), -cos(phi(1)), 0; cos(phi(1)), ...
+    -sin(phi(1)), 0; 0, 0, 0];
 C1 = (C(1,1)/A(1,1))^2 - k2*w2*Q(1,1)*C(1,1)/(w1*A(1,1)^2);
 C2 = (k2^2)*Q(1,1)/(2*w1*A(1,1)^2);
